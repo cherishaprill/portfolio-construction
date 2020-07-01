f3c696d6b54188ace3fccc03d137eaf49a18b88a
Yimiao <cherishaprill@gmail.com>
2020-07-01 22:59:08 +0100

    added stock return graph

--- returns.py
+++ returns.py
@@ -7,3 +7,11 @@ prices.iloc[1:]
 prices.iloc[:-1]
 returns = prices.iloc[1:].values/prices.iloc[:-1] - 1
 returns
+
+### read csv file 
+me_m=pd.read_csv("Desktop/portfolio_python/data/Portfolios_Formed_on_ME_monthly_EW.csv", 
+                 header=0, index_col=0, parse_dates=True, na_values=-99.99)
+rets=me_m[['Lo 10', 'Hi 10']]
+rets.columns = ['SmallCap', 'LargeCap']
+rets = rets/100
+rets.plot.line()
